e2bdd7134ca5ace48a169c3204c60e68f030d488
Felipe Gil Gutierrez <felipe.gil@arvato.com>
2018-07-25 12:23:02 +0200

    _lodash Function,Strings & Array

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const math = require('./lib/lodash-math')
+const date = require('./lib/lodash-date')
+const func = require('./lib/lodash-func')
 
 math.init()
+date.init()
+func.func()
+func.string()
